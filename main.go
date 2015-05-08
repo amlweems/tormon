307f0136718da3f30e8f437335003bc62cd911eb
Anthony Weems <amlweems@gmail.com>
2015-05-08 01:56:49 -0500

    Rewrite as HTTP server with constant refresh and support for paging

--- main.go
+++ main.go
@@ -2,16 +2,28 @@ package main
 
 import "fmt"
 import "log"
-import "net"
 import "time"
+import "net/http"
 import "strconv"
 import "os/exec"
 
 const screens int = 10
 
+var pageTable = map[string]int{
+    "/active":     0, "/0": 0,
+    "/main":       1, "/1": 1,
+    "/name":       2, "/2": 2,
+    "/started":    3, "/3": 3,
+    "/stopped":    4, "/4": 4,
+    "/complete":   5, "/5": 5,
+    "/incomplete": 6, "/6": 6,
+    "/hashing":    7, "/7": 7,
+    "/seeding":    8, "/8": 8,
+    "/leeching":   9, "/9": 9,
+}
+
 type CapturePane struct {
 	snap [screens][]byte
-	ts   time.Time
 }
 
 var pane CapturePane
@@ -24,6 +36,8 @@ func snapshot(screen int) []byte {
 		return []byte{}
 	}
 
+	time.Sleep(50 * time.Millisecond)
+
 	cmd := exec.Command("tmux", "capture-pane", "-t", "rtorrent", "-p")
 	stdout, err := cmd.Output()
 	if err != nil {
@@ -33,31 +47,32 @@ func snapshot(screen int) []byte {
 	return stdout
 }
 
-func handleConn(conn net.Conn) {
-	defer conn.Close()
-
-	now := time.Now()
-	if now.Sub(pane.ts) > 3*time.Second {
-		for i := 0; i < screens; i++ {
-			pane.snap[i] = snapshot(i)
-		}
-		pane.ts = now
+func update() {
+	for i := 0; i < screens; i++ {
+		pane.snap[i] = snapshot(i)
 	}
-	snap := pane.snap[5]
-	conn.Write(snap)
+}
+
+func handleRequest(w http.ResponseWriter, req *http.Request) {
+	index := pageTable[req.URL.Path]
+	w.Write(pane.snap[index])
 }
 
 func main() {
-	ln, err := net.Listen("tcp", ":4002")
-	if err != nil {
-		log.Fatal(err)
-	}
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			go handleConn(conn)
+	ticker := time.NewTicker(5 * time.Second)
+	quit := make(chan struct{})
+	go func() {
+		for {
+			select {
+				case <- ticker.C:
+					update()
+				case <- quit:
+					ticker.Stop()
+					return
+			}
 		}
-	}
+	}()
+
+	http.HandleFunc("/", handleRequest)
+	log.Fatal(http.ListenAndServe(":4002", nil))
 }
