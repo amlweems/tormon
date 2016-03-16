5928e15991d2b9a94bd08961879dc62afab17580
Anthony Weems <amlweems@gmail.com>
2016-03-16 00:04:22 -0500

    Remove the tabs, limit to localhost, resize on render

--- main.go
+++ main.go
@@ -5,72 +5,50 @@ import "log"
 import "time"
 import "net/http"
 import "strconv"
+import "os"
 import "os/exec"
 import "html/template"
-
-const screens int = 10
-
-var pageTable = map[string]int{
-	"/active": 0, "/0": 0,
-	"/main": 1, "/1": 1,
-	"/name": 2, "/2": 2,
-	"/started": 3, "/3": 3,
-	"/stopped": 4, "/4": 4,
-	"/complete": 5, "/5": 5,
-	"/incomplete": 6, "/6": 6,
-	"/hashing": 7, "/7": 7,
-	"/seeding": 8, "/8": 8,
-	"/leeching": 9, "/9": 9,
-}
-
-type CapturePane struct {
-	snap [screens][]byte
-}
+import "io/ioutil"
 
 type Page struct {
 	Title string
 	Body  []byte
-	Prev  int
-	Next  int
 }
 
-var pane CapturePane
+var pane []byte
 var ticker *time.Ticker
 
-func snapshot(screen int) []byte {
-	move := exec.Command("tmux", "send-keys", "-t", "rtorrent", strconv.Itoa(screen))
-	_, err := move.Output()
+func update() {
+	tempFile, err := ioutil.TempFile("/tmp", "tormon")
 	if err != nil {
 		fmt.Println(err)
-		return []byte{}
+		return
 	}
+	defer tempFile.Close()
+	defer os.Remove(tempFile.Name())
 
+	resize := exec.Command("screen", "-X", "width", "-d", "120", "250")
+	resize.Run()
+
+	proc := exec.Command("screen", "-X", "hardcopy", tempFile.Name())
+	proc.Run()
+
+	// hardcopy is non-blocking, we'll just wait a bit
 	time.Sleep(50 * time.Millisecond)
 
-	cmd := exec.Command("tmux", "capture-pane", "-t", "rtorrent", "-p")
-	stdout, err := cmd.Output()
+	pane, err = ioutil.ReadFile(tempFile.Name())
 	if err != nil {
 		fmt.Println(err)
-		return []byte{}
-	}
-	return stdout
-}
-
-func update() {
-	for i := 0; i < screens; i++ {
-		pane.snap[i] = snapshot(i)
+		return
 	}
 }
 
 func handleRequest(w http.ResponseWriter, req *http.Request) {
 	title := req.URL.Path[1:]
 
-	index := pageTable[req.URL.Path]
 	p := &Page{
 		Title: title,
-		Body:  pane.snap[index],
-		Prev:  (index - 1 + screens) % screens,
-		Next:  (index + 1) % screens,
+		Body:  pane,
 	}
 	t, _ := template.ParseFiles("template.html")
 	err := t.Execute(w, p)
@@ -103,5 +81,5 @@ func main() {
 
 	http.HandleFunc("/", handleRequest)
 	http.HandleFunc("/ticker/", handleTicker)
-	log.Fatal(http.ListenAndServe(":4002", nil))
+	log.Fatal(http.ListenAndServe("127.0.0.1:4002", nil))
 }
